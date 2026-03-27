b7b9d9a926be00df9528e488749deb9f8b8f9772
Giovanni Bindi <giovanni.bindi@gmail.com>
2026-03-27 19:07:09 +0100

    final?

---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import 'leaflet/dist/leaflet.css'
+import './index.css'
+import App from './App.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
+
+if ('serviceWorker' in navigator && import.meta.env.PROD) {
+  window.addEventListener('load', () => {
+    const serviceWorkerUrl = `${import.meta.env.BASE_URL}sw.js`
+    void navigator.serviceWorker.register(serviceWorkerUrl)
+  })
+}
